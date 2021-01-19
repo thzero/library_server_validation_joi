d0abced1e4f9f24c361eaf7482922e1f0675687e
thzero <thzero@users.noreply.github.com>
2021-01-19 00:45:46 -0600

    library update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import Joi from '@hapi/joi';
 import JoiDate from '@hapi/joi-date';
 Joi.extend(JoiDate);
 
-import BaseValidationService from '@thzero/library_server/service/validation';
+import BaseValidationService from '@thzero/library_common/service/validation';
 
 class JoiBaseValidationService extends BaseValidationService {
 	check(correlationId, schema, value, context, prefix) {
