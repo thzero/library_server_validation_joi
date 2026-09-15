4ad14258936f9b8f9e7b26c01b3dd3e5a2b9b4f5
thZero <corvette@thzero.com>
2026-09-14 22:03:56 -0500

    licecnse updates

--- openSource.js
+++ openSource.js
@@ -1,12 +1,12 @@
 export default () => {
     return [
-		{
-			category: 'server',
-			name: '@joi/date',
-			url: 'https://github.com/hapijs/joi-date',
-			licenseName: 'MIT',
-			licenseUrl: 'https://github.com/hapijs/joi-date/blob/master/LICENSE.md'
-		},
+        {
+            category: 'server',
+            name: '@joi/date',
+            url: 'https://github.com/hapijs/joi-date',
+            licenseName: 'BSD-3-Clause',
+            licenseUrl: 'https://github.com/hapijs/joi-date/blob/master/LICENSE.md'
+        },
         {
             category: 'server',
             name: '@thzero/library_common',
@@ -23,24 +23,31 @@ export default () => {
         },
         {
             category: 'server',
-            name: '@thzero/library_service',
-            url: 'https://github.com/thzero/library_service',
+            name: '@thzero/library_server',
+            url: 'https://github.com/thzero/library_server',
             licenseName: 'MIT',
-            licenseUrl: 'https://github.com/thzero/library_service/blob/master/license.md'
+            licenseUrl: 'https://github.com/thzero/library_server/blob/master/license.md'
         },
-		{
-			category: 'server',
-			name: '@thzero/library_server_validation_joi',
-			url: 'https://github.com/thzero/library_server_validation_joi',
-			licenseName: 'MIT',
-			licenseUrl: 'https://github.com/thzero/library_server_validation_joi/blob/master/license.md'
-		},
-		{
-			category: 'server',
-			name: 'joi',
-			url: 'https://github.com/hapijs/joi',
-			licenseName: 'MIT',
-			licenseUrl: 'https://github.com/hapijs/joi/blob/master/LICENSE.md'
-		}
+        {
+            category: 'server',
+            name: '@thzero/library_server_validation_joi',
+            url: 'https://github.com/thzero/library_server_validation_joi',
+            licenseName: 'MIT',
+            licenseUrl: 'https://github.com/thzero/library_server_validation_joi/blob/master/license.md'
+        },
+        {
+            category: 'server',
+            name: 'joi',
+            url: 'https://github.com/hapijs/joi',
+            licenseName: 'BSD-3-Clause',
+            licenseUrl: 'https://github.com/hapijs/joi/blob/master/LICENSE.md'
+        },
+        {
+            category: 'server',
+            name: 'moment',
+            url: 'https://github.com/moment/moment',
+            licenseName: 'MIT',
+            licenseUrl: 'https://github.com/moment/moment/blob/develop/LICENSE'
+        }
     ];
 }
